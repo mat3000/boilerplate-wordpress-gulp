d312272b9e31413076b5e55bbf04f9201e807842
mathieu <mat1024@gmail.com>
2017-10-12 08:06:27 +0200

    fix bug alert dev mode

--- wp-content/themes/Gulpfile.js
+++ wp-content/themes/Gulpfile.js
@@ -158,7 +158,7 @@ gulp.task('php', () => {
 	if(process.env.NODE_ENV){
 
 		let devtools  = '';
-			devtools += '<script type="text/javascript" id="dev-mode-82382">(function(){var s=document.createElement("style");s.type="text/css";s.id="dev-mode-93582";s.innerHTML="@keyframes dev-mode-alert{0%{opacity:0}50%{opacity:1}100%{opacity:0}}";document.body.appendChild(s);var d=document.createElement("div");d.id="dev-mode-29384";d.style="position:fixed;top:0;left:0;width:100%;height:2px;background:red;z-index:999999;opacity:0;animation: dev-mode-alert 1s linear;";document.body.appendChild(d);setTimeout(function(){d.remove();s.remove();document.getElementById("dev-mode-93582").remove()},2000)})();</script>';
+			devtools += '<script type="text/javascript" id="dev-mode-82382">(function(){var s=document.createElement("style");s.type="text/css";s.id="dev-mode-93582";s.innerHTML="@keyframes dev-mode-alert{0%{opacity:0}50%{opacity:1}100%{opacity:0}}";document.body.appendChild(s);var d=document.createElement("div");d.id="dev-mode-29384";d.style="position:fixed;top:0;left:0;width:100%;height:2px;background:red;z-index:999999;opacity:0;animation: dev-mode-alert 1s linear;";document.body.appendChild(d);setTimeout(function(){d.remove();s.remove();document.getElementById("dev-mode-82382").remove()},2000)})();</script>';
 			devtools += '<script src="http://log.matdev.fr/mylog.dev-6.0.0.js"></script>';
 	 	return gulp.src(['./src/**/*.php'])
 	 		.pipe(replace(/\{\@filename\@\}/g, 'bundle'))
